cb26f4923dd1e8d99c73c1a4df872beeb4befbea
yuzhiquan <yuzhiquan205643@sogou-inc.com>
2018-03-16 14:56:41 +0800

    :sunny: pep8 format code

--- flask_demo.py
+++ flask_demo.py
@@ -1,6 +1,6 @@
 import json
 
-from flask import Flask,request,jsonify
+from flask import Flask, request, jsonify
 from redis import Redis
 from rq import Queue
 
@@ -9,15 +9,18 @@ from tasks import write_log_file
 q = Queue(connection=Redis())
 app = Flask(__name__)
 
+
 @app.route('/ping')
 def hello_world():
-    return 'pong'
+	return 'pong'
 
-@app.route("/api/v1/log", methods = ["GET","POST"])
+
+@app.route("/api/v1/log", methods=["GET", "POST"])
 def log_storage():
-    if request.is_json:
-        q.enqueue(write_log_file,json.dumps(request.json),"flask_demo_test.log")
-    return jsonify({"err":"","msg":"succ"})
+	if request.is_json:
+		q.enqueue(write_log_file, json.dumps(request.json), "flask_demo_test.log")
+	return jsonify({"err": "", "msg": "succ"})
+
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=8090)
+	app.run(host="0.0.0.0", port=8090)
