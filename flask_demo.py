00cdca38a9c613a6292541849f7e1cecebfaafc2
yuzhiquan <yuzhiquan205643@sogou-inc.com>
2018-11-05 17:42:05 +0800

    :monkey: add photo upload api

--- flask_demo.py
+++ flask_demo.py
@@ -1,15 +1,16 @@
-import json
+import json,os
 
 from flask import Flask, request, jsonify
 from redis import Redis
 from rq import Queue
 
 from tasks import write_log_file
+from init_log import log
 
 q = Queue(connection=Redis())
 app = Flask(__name__)
 
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 @app.route('/ping')
 def hello_world():
 	return 'pong'
@@ -18,8 +19,19 @@ def hello_world():
 @app.route("/api/v1/log", methods=["GET", "POST"])
 def log_storage():
 	if request.is_json:
+                print json.dumps(request.json,indent=4)
 		q.enqueue(write_log_file, json.dumps(request.json), "flask_demo_test.log")
 	return jsonify({"err": "", "msg": "succ"})
+
+@app.route("/api/v1/photo", methods=["GET","POST"])
+def save_photo():
+    if request.method == "POST":
+        img=request.files.get("image")
+        log.info(dir(img))
+        img.save("./static/photo/"+img.filename)
+        return jsonify({"msg":"succ"})
+    elif request.method == "GET":
+        return jsonify({"msg":"list"})
 
 
 if __name__ == '__main__':
